f9c1663138656c9da1722d09f4c14e856a73f365
psgontarz <pgontarz@outlook.com>
2024-09-09 14:50:59 -0500

    Add project files.

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e;
+    int highest = 0;
+    cout << "Enter the value for a ==> ";
+    cin >> a;
+    cout << "Enter the value for b ==> ";
+    cin >> b;
+    cout << "Enter the value for c ==> ";
+    cin >> c;
+    cout << "Enter the value for d ==> ";
+    cin >> d;
+    cout << "Enter the value for e ==> ";
+    cin >> e;
+    if (a > highest)
+    {
+        highest = a;
+    }
+    if (b > highest)
+    {
+        highest = b;
+    }
+    if (c > highest)
+    {
+        highest = c;
+    }
+    if (d > highest)
+    {
+        highest = d;
+    }
+    if (e > highest)
+    {
+        highest = e;
+    }
+    cout << "\nThe Highest is ==> " << highest << endl;
+    int lowest = a;
+}
